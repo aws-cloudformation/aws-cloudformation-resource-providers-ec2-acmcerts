4ae48e55d2d5958f2e6210e81f83ea62a9318e2f
Alexandra Pirvulescu <alexprv@amazon.com>
2021-02-11 18:45:04 +0200

    Test that exceptions are handled correctly
    Signed-off-by: Alexandra Pirvulescu <alexprv@amazon.com>

--- aws-ec2-enclavecertificateiamroleassociation/src/test/java/software/amazon/ec2/enclavecertificateiamroleassociation/DeleteHandlerTest.java
+++ aws-ec2-enclavecertificateiamroleassociation/src/test/java/software/amazon/ec2/enclavecertificateiamroleassociation/DeleteHandlerTest.java
@@ -1,7 +1,9 @@
 package software.amazon.ec2.enclavecertificateiamroleassociation;
 
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.DisassociateEnclaveCertificateIamRoleRequest;
+import software.amazon.awssdk.services.ec2.model.GetAssociatedEnclaveCertificateIamRolesRequest;
 import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
@@ -111,5 +113,150 @@ public class DeleteHandlerTest extends AbstractTestBase {
         assertThat(response.getResourceModels()).isNull();
         assertThat(response.getMessage()).contains(ERROR_MESSAGE);
         assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
+    }
+
+    @Test
+    public void testDisassociateEnclaveCertIamRoleMalformedCertArn() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.MALFORMED_CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Malformed certificate arn message";
+        final AwsServiceException exception =
+                TestUtils.getException(400,
+                        BaseHandlerStd.ERROR_CODE_CERTIFICATE_ARN_MALFORMED,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .disassociateEnclaveCertificateIamRole(any(DisassociateEnclaveCertificateIamRoleRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).contains(ERROR_MESSAGE);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
+    }
+
+    @Test
+    public void testDisassociateEnclaveCertIamRoleMalformedRoleArn() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.MALFORMED_ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Malformed role arn message";
+        final AwsServiceException exception =
+                TestUtils.getException(400, BaseHandlerStd.ERROR_CODE_ROLE_ARN_MALFORMED,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .disassociateEnclaveCertificateIamRole(any(DisassociateEnclaveCertificateIamRoleRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).contains(ERROR_MESSAGE);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
+    }
+
+    @Test
+    public void testDisassociateEnclaveCertIamRoleUnauthorizedOperation() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Unauthorized operation message";
+        final AwsServiceException exception =
+                TestUtils.getException(403,
+                        BaseHandlerStd.ERROR_CODE_UNAUTHORIZED_OPERATION,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .disassociateEnclaveCertificateIamRole(any(DisassociateEnclaveCertificateIamRoleRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).contains(ERROR_MESSAGE);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.AccessDenied);
+    }
+
+    @Test
+    public void testDisassociateEnclaveCertIamRoleServerInternalError() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Server internal error message";
+        final AwsServiceException exception =
+                TestUtils.getException(500, BaseHandlerStd.ERROR_CODE_SERVER_INTERNAL,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .disassociateEnclaveCertificateIamRole(any(DisassociateEnclaveCertificateIamRoleRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InternalFailure);
+    }
+
+    @Test
+    public void testDisassociateEnclaveCertIamRoleServiceUnavailableError() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Service unavailable error message";
+        final AwsServiceException exception =
+                TestUtils.getException(500, BaseHandlerStd.ERROR_CODE_SERVICE_UNAVAILABLE,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .disassociateEnclaveCertificateIamRole(any(DisassociateEnclaveCertificateIamRoleRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.ServiceInternalError);
     }
 }
--- aws-ec2-enclavecertificateiamroleassociation/src/test/java/software/amazon/ec2/enclavecertificateiamroleassociation/ReadHandlerTest.java
+++ aws-ec2-enclavecertificateiamroleassociation/src/test/java/software/amazon/ec2/enclavecertificateiamroleassociation/ReadHandlerTest.java
@@ -1,5 +1,6 @@
 package software.amazon.ec2.enclavecertificateiamroleassociation;
 
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.GetAssociatedEnclaveCertificateIamRolesRequest;
 import software.amazon.cloudformation.proxy.ProxyClient;
@@ -77,6 +78,62 @@ public class ReadHandlerTest extends AbstractTestBase {
     }
 
     @Test
+    public void testGetEnclaveCertRoleArnAssociationEmptyResponse() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(model)
+                .build();
+
+        when(proxyClient.client()
+                .getAssociatedEnclaveCertificateIamRoles(any(GetAssociatedEnclaveCertificateIamRolesRequest.class)))
+                .thenReturn(TestUtils.createGetAssociationsEmptyResponse());
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, logger);
+
+        final String ERROR_MESSAGE = String.format("No association found for certificate arn %s and" +
+                        " role arn %s", TestUtils.CERTIFICATE_ARN, TestUtils.ROLE_ARN);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).contains(ERROR_MESSAGE);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.NotFound);
+    }
+
+    @Test
+    public void testGetEnclaveCertRoleArnAssociationNotFound() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(model)
+                .build();
+
+        when(proxyClient.client()
+                .getAssociatedEnclaveCertificateIamRoles(any(GetAssociatedEnclaveCertificateIamRolesRequest.class)))
+                .thenReturn(TestUtils.createGetAssociationsResponseRoleNotFound());
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, logger);
+
+        final String ERROR_MESSAGE = String.format("No association found for certificate arn %s and" +
+                " role arn %s", TestUtils.CERTIFICATE_ARN, TestUtils.ROLE_ARN);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).contains(ERROR_MESSAGE);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.NotFound);
+    }
+
+    @Test
     public void testGetEnclaveCertRoleArnAssociationWithoutCert() {
         final ResourceModel model = ResourceModel.builder()
                 .roleArn(TestUtils.ROLE_ARN)
@@ -114,5 +171,121 @@ public class ReadHandlerTest extends AbstractTestBase {
         assertThat(response.getResourceModels()).isNull();
         assertThat(response.getMessage()).contains(ERROR_MESSAGE);
         assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
+    }
+
+    @Test
+    public void testGetEnclaveCertIamRoleAssociationMalformedCertArn() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.MALFORMED_CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Malformed certificate arn message";
+        final AwsServiceException exception =
+                TestUtils.getException(400,
+                        BaseHandlerStd.ERROR_CODE_CERTIFICATE_ARN_MALFORMED,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .getAssociatedEnclaveCertificateIamRoles(any(GetAssociatedEnclaveCertificateIamRolesRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).contains(ERROR_MESSAGE);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InvalidRequest);
+    }
+
+    @Test
+    public void testGetEnclaveCertIamRoleAssociationUnauthorizedOperation() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Unauthorized operation message";
+        final AwsServiceException exception =
+                TestUtils.getException(403,
+                        BaseHandlerStd.ERROR_CODE_UNAUTHORIZED_OPERATION,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .getAssociatedEnclaveCertificateIamRoles(any(GetAssociatedEnclaveCertificateIamRolesRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getMessage()).contains(ERROR_MESSAGE);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.AccessDenied);
+    }
+
+    @Test
+    public void testGetEnclaveCertIamRoleAssociationServerInternalError() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Server internal error message";
+        final AwsServiceException exception =
+                TestUtils.getException(500, BaseHandlerStd.ERROR_CODE_SERVER_INTERNAL,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .getAssociatedEnclaveCertificateIamRoles(any(GetAssociatedEnclaveCertificateIamRolesRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.InternalFailure);
+    }
+
+    @Test
+    public void testGetEnclaveCertIamRoleAssociationServiceUnavailableError() {
+        final ResourceModel model = ResourceModel.builder()
+                .certificateArn(TestUtils.CERTIFICATE_ARN)
+                .roleArn(TestUtils.ROLE_ARN)
+                .build();
+        final ResourceHandlerRequest<ResourceModel> request =
+                TestUtils.createAssociationRequest(model);
+
+        final String ERROR_MESSAGE = "Service unavailable error message";
+        final AwsServiceException exception =
+                TestUtils.getException(500, BaseHandlerStd.ERROR_CODE_SERVICE_UNAVAILABLE,
+                        ERROR_MESSAGE);
+
+        when(proxyClient.client()
+                .getAssociatedEnclaveCertificateIamRoles(any(GetAssociatedEnclaveCertificateIamRolesRequest.class)))
+                .thenThrow(exception);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response =
+                handler.handleRequest(proxy, request, null, proxyClient, logger);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
+        assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.ServiceInternalError);
     }
 }

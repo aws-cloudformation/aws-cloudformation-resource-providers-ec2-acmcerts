1dd5a5bdf513e069cc5f2d83a4ab6f733c134388
Alexandra Pirvulescu <alexprv@amazon.com>
2021-02-25 19:23:30 +0200

    Fix DeleteHandler
    DeleteHandler should return an error if the association
    doesn't exist.
    
    Signed-off-by: Alexandra Pirvulescu <alexprv@amazon.com>

--- aws-ec2-enclavecertificateiamroleassociation/src/main/java/software/amazon/ec2/enclavecertificateiamroleassociation/DeleteHandler.java
+++ aws-ec2-enclavecertificateiamroleassociation/src/main/java/software/amazon/ec2/enclavecertificateiamroleassociation/DeleteHandler.java
@@ -9,6 +9,7 @@ import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.HandlerErrorCode;
 
 public class DeleteHandler extends BaseHandlerStd {
     @Override
@@ -22,6 +23,18 @@ public class DeleteHandler extends BaseHandlerStd {
         try {
             validateNotNull(model.getCertificateArn(), Properties.CertificateArn);
             validateNotNull(model.getRoleArn(), Properties.RoleArn);
+
+            if (Helper.getAssociatedRole(proxyClient, model.getCertificateArn(),
+                    model.getRoleArn()) == null) {
+                return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                        .resourceModel(model)
+                        .message(String.format("No association found for certificate arn %s and" +
+                                        " role arn %s",
+                                model.getCertificateArn(), model.getRoleArn()))
+                        .status(OperationStatus.FAILED)
+                        .errorCode(HandlerErrorCode.NotFound)
+                        .build();
+            }
 
             final DisassociateEnclaveCertificateIamRoleRequest disassociateRequest =
                     DisassociateEnclaveCertificateIamRoleRequest
